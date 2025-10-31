27e73523c8f26b76167710b31e468a1a8b1a590b
Prankur Sharma <prankur@example.com>
2025-10-31 19:08:28 +0530

    new commit

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios"
 
+const baseURL = process.env.REACT_APP_API_URL || "https://travel-website-7vcf.onrender.com/api";
+
+console.log("API Base URL:", baseURL);
+
 const api = axios.create({
-    baseURL: "https://travel-website-7vcf.onrender.com/api",
+    baseURL: baseURL,
 });
 
 export default api;
